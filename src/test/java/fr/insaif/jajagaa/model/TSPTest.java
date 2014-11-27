a8dcbce318c41b621874c2f70adbde20340b5278
JeanMarchal <jean.marchal@hotmail.com>
2014-11-27 13:27:58 +0100

    modification de l'ajout des tronçons dans TSPTest.java

--- src/test/java/fr/insaif/jajagaa/model/TSPTest.java
+++ src/test/java/fr/insaif/jajagaa/model/TSPTest.java
@@ -34,13 +34,13 @@ public class TSPTest {
 
         // Exemple de tronçons
         //TODO 
-        /*noeuds.get(0).addSortant(noeuds.get(1), 349f, 3.32f);
-        noeuds.get(0).addSortant(noeuds.get(3), 123.5f, 4.43f); // min
-        noeuds.get(1).addSortant(noeuds.get(2), 312.4f, 6.831f); // max
-        noeuds.get(2).addSortant(noeuds.get(3), 323.5f, 3.43f);
-        noeuds.get(3).addSortant(noeuds.get(0), 432.4f, 1.43f);
+        noeuds.get(0).addSortant(noeuds.get(1), 349f, 3.32f, "a");
+        noeuds.get(0).addSortant(noeuds.get(3), 123.5f, 4.43f, "b"); // min
+        noeuds.get(1).addSortant(noeuds.get(2), 312.4f, 6.831f, "c"); // max
+        noeuds.get(2).addSortant(noeuds.get(3), 323.5f, 3.43f, "c");
+        noeuds.get(3).addSortant(noeuds.get(0), 432.4f, 1.43f, "d");
 
-        this.graph = new LivraisonGraph(noeuds);*/
+        //this.graph = new LivraisonGraph(noeuds);
     }
 
     @Test
