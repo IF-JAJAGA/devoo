bd1311848a4730d909fdfddc386c91292d13354c
albertorevel <alberto.revel@gmail.com>
2014-11-28 09:58:27 +0100

    Correction Parseur. Ajout Tests (pas finis)

--- src/test/java/fr/insaif/jajagaa/control/ParseurTest.java
+++ src/test/java/fr/insaif/jajagaa/control/ParseurTest.java
@@ -1,15 +1,13 @@
 package fr.insaif.jajagaa.control;
 
-import fr.insaif.jajagaa.model.Livraison;
-import fr.insaif.jajagaa.model.PlageHoraire;
-import fr.insaif.jajagaa.model.ZoneGeographique;
-import org.junit.Test;
-
 import java.io.FileInputStream;
 import java.text.SimpleDateFormat;
 import java.util.List;
 
-import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+import fr.insaif.jajagaa.model.Noeud;
+import fr.insaif.jajagaa.model.ZoneGeographique;
 
 /**
  * Test de {@link fr.insaif.jajagaa.control.Parseur}
@@ -19,11 +17,11 @@ public class ParseurTest {
     /**
      * Nombre de livraisons dans le fichier de test
      */
-    protected static final int NB_LIVRAISON_1 = 8;
-
+	//TODO Test plan10x10, Test plan20x20, Test xml mal formé, Test xml n'existe pas
+	//XML mar formé: manque d'attributs, Noeuds pas fermés
     @Test
     public void testLireLivraison() throws Exception {
-        /*
+        
         FileInputStream inputStream = null;
         FileInputStream inputStreamPlan = null;
         ZoneGeographique zone = null;
@@ -32,16 +30,17 @@ public class ParseurTest {
             inputStream = new FileInputStream("./src/test/resources/livraison10x10-1.xml");
             inputStreamPlan = new FileInputStream("./src/test/resources/plan10x10.xml");
             zone = Parseur.lirePlan(inputStreamPlan);
-            List<PlageHoraire> plages = Parseur.lireLivraison(inputStream,zone);
-            for (PlageHoraire plage : plages) {
-                List<Livraison> livraisons = plage.getLivraisons();
-                assertEquals(NB_LIVRAISON_1, livraisons.size());
-                assertEquals(simpleDateFormat.parse("8:0:0"), livraisons.get(0).getHeureLivraison());
-                //assertEquals(simpleDateFormat.parse("12:0:0"), livraisons.get(0).getHeureLivraison());
-                for (int i = 1; i <= NB_LIVRAISON_1; ++i) {
-                    assertEquals(i, livraisons.get(i - 1).getPointLivraison().getId());
-                }
-            }
+            List<Noeud> listNoeuds = zone.getNoeuds();
+//            List<PlageHoraire> plages = Parseur.lireLivraison(inputStream,zone);
+//            for (PlageHoraire plage : plages) {
+//                List<Livraison> livraisons = plage.getLivraisons();
+//                assertEquals(NB_LIVRAISON_1, livraisons.size());
+//                assertEquals(simpleDateFormat.parse("8:0:0"), livraisons.get(0).getHeureLivraison());
+//                assertEquals(simpleDateFormat.parse("12:0:0"), livraisons.get(0).getHeureLivraison());
+//                for (int i = 1; i <= NB_LIVRAISON_1; ++i) {
+//                    assertEquals(i, livraisons.get(i - 1).getPointLivraison().getId());
+//                }
+//            }
             
         } finally {
             if (inputStream != null) {
@@ -51,6 +50,6 @@ public class ParseurTest {
                 inputStreamPlan.close();
             }
         }
-        */
+       
     }
 }
