4bb659eed61fca39ccf39fbb064fecfa195c03cf
JeanMarchal <jean.marchal@hotmail.com>
2014-11-30 17:18:45 +0100

    Ajout de javadoc dans Controleur.java

--- src/main/java/fr/insaif/jajagaa/control/Controleur.java
+++ src/main/java/fr/insaif/jajagaa/control/Controleur.java
@@ -17,6 +17,11 @@ import java.util.List;
  */
 public class Controleur {
     private static Controleur controleur;
+    
+    /**
+     * Singleton pour la création d'un Controleur
+     * @return instance du controleur si existant sinon en crée un nouveau
+     */
     public static Controleur getInstance(){
         if(controleur==null){  
             controleur = new Controleur();
@@ -33,23 +38,50 @@ public class Controleur {
         Fenetre.getInstance();
     }
 
+    /**
+     * Getteur de ZoneGeographique 
+     * @return ZoneGeographique associée au controleur en question
+     */
     public ZoneGeographique getZone() {
         return zone;
     }
 
+    /**
+     * Getteur de la liste de PlageHoraire
+     * @return List<PlageHoraire> associée au controleur en question
+     */
     public List<PlageHoraire> getPlagesHoraire() {
         return plagesHoraire;
     }
     
+    /**
+     * Méthode permettant de créer une tournée à partir du plan et de la liste de livraison passés en paramètre
+     * @param fichierPlan
+     * @param fichierLivraison
+     * @throws FileNotFoundException 
+     */
     public void creerTournee(FileInputStream fichierPlan, FileInputStream fichierLivraison) throws FileNotFoundException {
         ZoneGeographique zone = Parseur.lirePlan(fichierPlan);
         List<PlageHoraire> livraisons = Parseur.lireLivraison(fichierLivraison, zone);
     }
+    /**
+     * Méthode permettant de générer la zone géographique correspondant à la liste de noeuds listés dans le fichier xml passé en paramètre
+     * @param fichierPlan
+     * @return
+     * @throws FileNotFoundException 
+     */
     public ZoneGeographique lirePlan(String fichierPlan) throws FileNotFoundException {
         this.zone = Parseur.lirePlan(new FileInputStream(fichierPlan));
         return this.zone;
     }
     
+    /**
+     * Méthode permettant de générer la liste des plages horaires contenant les ensembles de livraisons à réaliser
+     * @param fichierLivraison
+     * @param zone
+     * @return
+     * @throws FileNotFoundException 
+     */
     public List<PlageHoraire> lireLivraisons(String fichierLivraison, ZoneGeographique zone) throws FileNotFoundException {
         this.plagesHoraire = Parseur.lireLivraison(new FileInputStream(fichierLivraison), zone);
         return this.plagesHoraire;
@@ -62,7 +94,7 @@ public class Controleur {
      * @param noeudAvant noeud après lequel on veut ajouter noeudMilieu
      * @return la tournee une fois qu'elle a été modifiée
      */
-      public Tournee ajouterPointLivraison (Tournee tourneeModel,Noeud noeudMilieu, Noeud noeudAvant) {
+    public Tournee ajouterPointLivraison (Tournee tourneeModel,Noeud noeudMilieu, Noeud noeudAvant) {
         return null;
     }
       
