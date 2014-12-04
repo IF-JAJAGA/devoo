8c91db651f970e8505f40976952ceb92f40c704c
JeanMarchal <jean.marchal@hotmail.com>
2014-12-04 17:25:35 +0100

    Javadoc de ParseurException.java

--- src/main/java/fr/insaif/jajagaa/control/ParseurException.java
+++ src/main/java/fr/insaif/jajagaa/control/ParseurException.java
@@ -1,7 +1,16 @@
 package fr.insaif.jajagaa.control;
 
+/**
+ * Classe désignant les exceptions relevées par le Parseur
+ * @author alberto
+ */
 public class ParseurException extends Exception{
 	
+        /**
+         * Création de l'exception qui devra être renvoyé avec le message passé 
+         * en paramètre
+         * @param message 
+         */
 	public ParseurException(String message){
 		super(message);
 	}
